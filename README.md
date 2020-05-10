2a4e19b0030e2ed025e66be8dc0bb1d8bf271562
Sergio Garcia <lawfulhacker@gmail.com>
2020-05-10 21:08:04 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Get the Last Deployment
+
+This action simples call GitHub API to get the last deployment in a environment
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function run(): Promise<void> {
+  try {
+    const token = (core.getInput('github_token') ||
+      process.env.GITHUB_TOKEN) as string
+
+    const octokit = new github.GitHub(token)
+    const context = github.context
+
+    const environment = core.getInput('environment')
+
+    const request = await octokit.repos.listDeployments({
+      ...context.repo,
+      environment
+    })
+
+    const deployments = request.data
+
+    if (deployments.length >= 0) {
+      core.setOutput('deployment_id', deployments[0].id.toString())
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
